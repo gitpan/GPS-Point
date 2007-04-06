3b11f244a363db0ef3bd66da89482163906842f3
Michael R. Davis <mrdvt@cpan.org>
2007-04-06 22:33:18 +0000

    Import of MRDVT/GPS-Point-0.03 from CPAN.
    gitpan-cpan-distribution: GPS-Point
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         MRDVT/GPS-Point-0.03.tar.gz
    gitpan-cpan-author:       MRDVT
    gitpan-cpan-maturity:     released

--- lib/GPS/Point.pm
+++ lib/GPS/Point.pm
@@ -3,7 +3,7 @@ use strict;
 
 BEGIN {
     use vars qw($VERSION);
-    $VERSION     = '0.02';
+    $VERSION     = '0.03';
 }
 
 =head1 NAME
@@ -14,31 +14,37 @@ GPS::Point - Provides an object interface for a GPS point.
 
   use GPS::Point;
   $obj=GPS::Point->new(
+         time=>$time,       #float seconds from the unix epoch
          lat=>$lat,         #signed degrees
          lon=>$lon,         #signed degrees
          alt=>$hae,         #meters above the WGS-84 ellipsoid
          speed=>$speed,     #meters/second (over ground)
          heading=>$heading, #degrees clockwise from North
-         time=>$time,       #float seconds of the unix epoch
+         climb=>$climb,     #meters/second
          etime=>$etime,     #float seconds
          ehorizontal=>$ehz, #float meters
          evertical=$evert,  #float meters
-         climb=>$climb,     #meters/second
-         eheading=>$ehead,  #degrees
          espeed=>$espeed,   #meters/second
+         eheading=>$ehead,  #degrees
          eclimb=>$eclimb,   #meters/second
-         mode=>$mode,       #[Unknown=>undef(),None=>1,2D=>2,3D=>3]
+         mode=>$mode,       #GPS mode [Unknown=>undef(),None=>1,2D=>2,3D=>3]
          tag=>$tag,         #Name of the GPS message for data
        ); 
   print $point->latlon. "\n";      #use a "." here to force latlon to a scalar
-  my ($x,$y,$z)=$point->ecef;      #if Geo::ECEF is available
-  my $GeoPoint=$point->GeoPoint;   #if Geo::Point is available
-  my $obj=GPS::Point->newGPSD($O_Command_Return);
+  my ($x,$y,$z)=$point->ecef;               #if Geo::ECEF is available
+  my $GeoPoint=$point->GeoPoint;            #if Geo::Point is available
+  my $distance=$point->distance($point2);   #if Geo::Inverse is available
+
+=head1 SYNOPSIS TODO
+
+  my $obj=GPS::Point->newGPSD($GPSD_O_line);#e.g. GPSD,O=....
+  my $obj=GPS::Point->newNMEA($NMEA_lines); #e.g. GGA+GSA+RMC
 
 =head1 DESCRIPTION
 
-This is a re-write of Net::GPSD::Point this is more portable.  
-GPS::Point - Provides an object interface for a GPS fix (e.g. Position, Velocity and Time).  (Note: Please use Geo::Point, if you want 2D or projection supportonly.)
+This is a re-write of Net::GPSD::Point that is more portable.
+
+GPS::Point - Provides an object interface for a GPS fix (e.g. Position, Velocity and Time).  (Note: Please use Geo::Point, if you want 2D or projection support.)
 
 =head1 USAGE
 
@@ -46,7 +52,7 @@ GPS::Point - Provides an object interface for a GPS fix (e.g. Position, Velocity
 
 =head2 new
 
-  my $obj = STOP::Device::BluTag->new();
+  my $obj = GPS::Point->new();
 
 =cut
 
@@ -68,9 +74,23 @@ sub initialize {
   %$self=@_;
 }
 
+=head2 time
+
+Sets or returns time
+
+  print $obj->time, "\n";
+
+=cut
+
+sub time {
+  my $self = shift();
+  $self->{'time'}=shift() if @_;
+  return $self->{'time'};
+}
+
 =head2 lat
 
-Sets of returns lat
+Sets or returns lat
 
   print $obj->lat, "\n";
 
@@ -84,7 +104,7 @@ sub lat {
 
 =head2 lon
 
-Sets of returns lon
+Sets or returns lon
 
   print $obj->lon, "\n";
 
@@ -98,7 +118,7 @@ sub lon {
 
 =head2 alt
 
-Sets of returns alt
+Sets or returns alt
 
   print $obj->alt, "\n";
 
@@ -112,7 +132,7 @@ sub alt {
 
 =head2 speed
 
-Sets of returns speed
+Sets or returns speed
 
   print $obj->speed, "\n";
 
@@ -126,7 +146,7 @@ sub speed {
 
 =head2 heading
 
-Sets of returns heading
+Sets or returns heading
 
   print $obj->heading, "\n";
 
@@ -138,23 +158,23 @@ sub heading {
   return $self->{'heading'};
 }
 
-=head2 time
+=head2 climb
 
-Sets of returns time
+Sets or returns climb
 
-  print $obj->time, "\n";
+  print $obj->climb, "\n";
 
 =cut
 
-sub time {
+sub climb {
   my $self = shift();
-  $self->{'time'}=shift() if @_;
-  return $self->{'time'};
+  $self->{'climb'}=shift() if @_;
+  return $self->{'climb'};
 }
 
 =head2 etime
 
-Sets of returns etime
+Sets or returns etime
 
   print $obj->etime, "\n";
 
@@ -168,7 +188,7 @@ sub etime {
 
 =head2 ehorizontal
 
-Sets of returns ehorizontal
+Sets or returns ehorizontal
 
   print $obj->ehorizontal, "\n";
 
@@ -182,7 +202,7 @@ sub ehorizontal {
 
 =head2 evertical
 
-Sets of returns evertical
+Sets or returns evertical
 
   print $obj->evertical, "\n";
 
@@ -194,37 +214,9 @@ sub evertical {
   return $self->{'evertical'};
 }
 
-=head2 climb
-
-Sets of returns climb
-
-  print $obj->climb, "\n";
-
-=cut
-
-sub climb {
-  my $self = shift();
-  $self->{'climb'}=shift() if @_;
-  return $self->{'climb'};
-}
-
-=head2 eheading
-
-Sets of returns eheading
-
-  print $obj->eheading, "\n";
-
-=cut
-
-sub eheading {
-  my $self = shift();
-  $self->{'eheading'}=shift() if @_;
-  return $self->{'eheading'};
-}
-
 =head2 espeed
 
-Sets of returns espeed
+Sets or returns espeed
 
   print $obj->espeed, "\n";
 
@@ -236,9 +228,23 @@ sub espeed {
   return $self->{'espeed'};
 }
 
+=head2 eheading
+
+Sets or returns eheading
+
+  print $obj->eheading, "\n";
+
+=cut
+
+sub eheading {
+  my $self = shift();
+  $self->{'eheading'}=shift() if @_;
+  return $self->{'eheading'};
+}
+
 =head2 eclimb
 
-Sets of returns eclimb
+Sets or returns eclimb
 
   print $obj->eclimb, "\n";
 
@@ -252,7 +258,7 @@ sub eclimb {
 
 =head2 mode
 
-Sets of returns mode
+Sets or returns GPS fix mode
 
   print $obj->mode, "\n";
 
@@ -266,7 +272,7 @@ sub mode {
 
 =head2 tag
 
-Sets of returns tag
+Sets or returns tag
 
   print $obj->tag, "\n";
 
@@ -293,6 +299,68 @@ sub latlon {
   return wantarray ? @latlon : join(" ", @latlon);
 }
 
+=head2 ecef
+
+Returns ECEF coordinates
+
+  my ($x,$y,$z) = $point->ecef;
+  my @xyz       = $point->ecef;
+  my $xyz       = $point->ecef; #[] if Geo::ECEF::VERSION >= 0.08
+
+=cut
+
+sub ecef {
+  my $self = shift();
+  eval 'use Geo::ECEF';
+  if ($@) {
+    die("Error: The ecef method requires Geo::ECEF");
+  } else {
+    my $obj=Geo::ECEF->new();
+    return $obj->ecef($self->lat, $self->lat, $self->alt);
+  }
+}
+
+=head2 GeoPoint
+
+Returns GeoPoint Object
+
+  my $gp = $point->GeoPoint;
+
+=cut
+
+sub GeoPoint {
+  my $self = shift();
+  eval 'use Geo::Point';
+  if ($@) {
+    die("Error: The GeoPoint method requires Geo::Point");
+  } else {
+    return Geo::Point->new(lat=>$self->lat, long=>$self->lon, proj=>'wgs84');
+  }
+}
+
+=head2 distance
+
+Returns distance
+
+  my $gp = $point->distance($pt2);
+  my $gp = $point->distance($lat, $lon);
+
+=cut
+
+sub distance {
+  my $self = shift();
+  eval 'use Geo::Inverse';
+  if ($@) {
+    die("Error: The distance method requires Geo::Inverse");
+  } else {
+    my $obj = Geo::Inverse->new(); # default "WGS84"
+    my $pt2=shift();
+    my ($lat1,$lon1,$lat2,$lon2)=($self->lat, $self->lon, $pt2->lat, $pt2->lon);
+    my ($faz, $baz, $dist)=$obj->inverse($lat1,$lon1,$lat2,$lon2);
+    return wantarray ? ($faz, $baz, $dist) : $dist;
+  }
+}
+
 =head1 BUGS
 
 =head1 SUPPORT
@@ -316,7 +384,7 @@ LICENSE file included with this module.
 
 =head1 SEE ALSO
 
-L<Geo::Point>, L<Net::GPSD>
+L<Geo::Point>, L<Net::GPSD::Point>, L<Geo::Distance>, L<Geo::ECEF>, L<Geo::Functions>
 
 =cut
 
--- t/002_init.t
+++ t/002_init.t
@@ -2,13 +2,62 @@
 
 # t/001_load.t - check module loading and create testing directory
 
-use Test::More tests => 3;
+sub near {
+  my $x=shift();
+  my $y=shift();
+  
+  return ($x-$y)/$y -1  <= 0.001;
+}
+use Test::More tests => 16;
+use blib;
 
 BEGIN { use_ok( 'GPS::Point' ); }
 
-my $object = GPS::Point->new(lat=>39,
-                             lon=>-77);
-isa_ok ($object, 'GPS::Point');
+my $pt1 = GPS::Point->new(lat=>39,
+                          lon=>-77);
+isa_ok ($pt1, 'GPS::Point');
 
-my $latlon=$object->latlon;
+my $latlon=$pt1->latlon;
 is($latlon, "39 -77", "latlon method");
+
+is($pt1->lat,  39, "lat");
+is($pt1->lon, -77, "lon");
+
+my $pt2 = GPS::Point->new(lat=>39.1,
+                          lon=>-77.1);
+isa_ok ($pt2, 'GPS::Point');
+
+SKIP: {
+  eval { require Geo::Inverse };
+
+  skip "Geo::Inverse not installed", 4 if $@;
+
+  my $dist=$pt1->distance($pt2);
+  is( $dist, "14077.7169524386", "distance" );
+  my @dist=$pt1->distance($pt2);
+  is( near($dist[0], "322.08605713267"), 1, "faz" );
+  is( near($dist[1], "142.02305726502"), 1, "baz" );
+  is( near($dist[2], "14077.7169524386"), 1, "distance" );
+}
+
+SKIP: {
+  eval { require Geo::Point };
+
+  skip "Geo::Point not installed", 3 if $@;
+
+  my $pt=$pt1->GeoPoint;
+  is( ref($pt),  "Geo::Point", "GeoPoint" );
+  is( $pt->lat,   "39", "GeoPoint->lat" );
+  is( $pt->long, "-77", "GeoPoint->long" );
+}
+
+SKIP: {
+  eval { require Geo::ECEF };
+
+  skip "Geo::ECEF not installed", 3 if $@;
+
+  my @xyz=$pt1->ecef;
+  is( near($xyz[0], "3857229.79658403"), 1, "ecef x" );
+  is( near($xyz[1], "3123523.10163777"), 1, "ecef y" );
+  is( near($xyz[2], "3992317.02275173"), 1, "ecef z" );
+}
